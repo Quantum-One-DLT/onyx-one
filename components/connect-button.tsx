107fb8bc435b82619218bf9842da9fdadf3de562
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-03-28 16:00:04 -0400

    Update connect-button.tsx

--- components/connect-button.tsx
+++ components/connect-button.tsx
@@ -1,14 +1,4 @@
-import { useWeb3Modal } from '@web3modal/wagmi/react'
-import { buttonVariants } from "@/components/ui/button"
 
 export default function ConnectButton() {
-  // 4. Use modal hook
-  const { open } = useWeb3Modal()
-
-  return (
-    <>
-      <button onClick={() => open()}>Connect</button>
-      <button onClick={() => open({ view: 'Networks' })}>Networks</button>
-    </>
-  )
+  return <w3m-button />
 }
