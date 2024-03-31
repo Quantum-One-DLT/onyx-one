fe2936720f24c05a35bc80d8c67009d084ce5199
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-03-31 13:21:13 -0400

    Update index.tsx

--- context/index.tsx
+++ context/index.tsx
@@ -19,8 +19,18 @@ createWeb3Modal({
   wagmiConfig: config,
   projectId,
   enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  enableOnramp: true // Optional - false as default
-})
+  enableOnramp: true,
+  
+  tokens: {
+    1: {
+      address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+      image: 'token_image_url' //optional
+    },
+    1: {
+      address: '0xeaaD65885fEA47a3B1258935f4Ce83aaB06FDD3A',
+      image: 'https://etherscan.io/token/images/quantumone_32.png' //optional
+    }
+  }})
 
 export default function Web3ModalProvider({
   children,
