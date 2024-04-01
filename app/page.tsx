0ccf4584a05d3ad1480e636e3bda2b8bd20f5bd9
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-01 16:00:50 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default async function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Onyx <br className="hidden sm:inline" />
-          embedded systems.
+          Uniswap v3 <br className="hidden sm:inline" />
+          On Zetachain. 
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-         Micro FinTech. 
+         DeFiOne
         </p>
       </div>
       <div className="flex gap-4">
