dc6b199673dca9f1eca6bbbaafcfc597062d3767
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-01 12:57:59 -0400

    Update index.tsx

--- config/index.tsx
+++ config/index.tsx
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 
 import { cookieStorage, createStorage } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { mainnet, sepolia, optimism } from 'wagmi/chains'
 
 // Get projectId at https://cloud.walletconnect.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
@@ -9,14 +9,14 @@ export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
 if (!projectId) throw new Error('Project ID is not defined')
 
 const metadata = {
-  name: 'Onyx',
-  description: 'Onyx One',
+  name: 'Quantum One DAO',
+  description: 'Humanity's new opportunity paradigm',
   url: 'https://onyx-one-two.vercel.app', // origin must match your domain & subdomain
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
 // Create wagmiConfig
-const chains = [mainnet, sepolia] as const
+const chains = [mainnet, sepolia, optimism] as const
 export const config = defaultWagmiConfig({
   chains,
   projectId,
