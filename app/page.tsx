eef4f8f8ed61bc54f908b976a12be37a1a18d2da
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-03 13:48:21 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default async function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          DeFi ONE <br className="hidden sm:inline" />
-          Finance meets freedom.  
+          DeFi ONE  <br className="hidden sm:inline" />
+         Coming soon.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-         A Quantum One DAO company.
+         Finance meets freedom. 
         </p>
       </div>
       <div className="flex gap-4">
