b207326c87f64d9d8c8430d498e3644e2226cbac
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-06 14:13:43 -0400

    Update app

---
+++ app/profile/page.tsx
@@ -0,0 +1,22 @@
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { ProfileForm } from '@/components/forms/profile-form'
+import { createClient } from '@/utils/supa-server-actions'
+
+export default async function PrivatePage() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect('/')
+  }
+
+  return 
+<>
+       <div className="flex text-center items-center">
+       <p>Hello {data.user.email}</p>
+      
+     <ProfileForm/>
+</>
+}
