8204ea270d23bb30319e3da5529bacb41f276836
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-06 16:58:23 -0400

    Update PricingPage.tsx

--- app/tokensale/PricingPage.tsx
+++ app/tokensale/PricingPage.tsx
@@ -18,8 +18,8 @@ export interface PricingTier {
   name: string;
   id: string;
   href: string;
-  discountPrice: string | Object<string, string>;
-  price: string | Object<string, string>,
+  discountPrice: string | Object<K, V>;
+  price: string | Object<K, V>; 
   description: string | React.ReactNode;
   features: string[];
   featured?: boolean;
