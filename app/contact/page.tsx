5b5cf887ba8004ff1f3a378c6c5d0b6a303236b8
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-06 21:08:09 -0400

    Update app

---
+++ app/contact/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ContactForm } from "@/components/forms/contact-form";
+import { readUserSession } from "@/utils/actions";
+import { redirect } from "next/navigation";
+
+export default async function ContactPage() {
+        const { data: userSession } = await readUserSession();
+
+        if (userSession.session) {
+         return (
+                <div className="flex items-center justify-center h-screen">
+                        <ContactForm />
+                </div>
+        );
+
+ return redirect("/auth");
+        }
+        
+}
