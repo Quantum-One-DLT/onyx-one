f6057e339c6b0899e929e6834d315526fa2f30a9
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-07 23:38:01 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,11 @@ export default async function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          DAO token sale! <br className="hidden sm:inline" />
-        </h1>
+          The quantum era <br className="hidden sm:inline" />
+          DAO
+	</h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-        Learn how you can buy our token and help deploy DeFi ONE on Zetachain!
+        Sign up to learn how our token sale will help bring DeFi ONE to Zetachain! Details coming soon!
         </p>
       </div>
       <div className="flex gap-4">
