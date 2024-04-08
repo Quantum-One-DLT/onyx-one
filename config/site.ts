0bc6343a3dcd12bc85cc3bc3110b13b38e82244d
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-08 00:35:56 -0400

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -10,8 +10,8 @@ export const siteConfig = {
       href: "/",
     },
     { 
-      title: "Dashboard",
-      href: "/dashboard",
+      title: "Profile",
+      href: "/profile",
     },
   ],
   links: {
@@ -19,7 +19,7 @@ export const siteConfig = {
     github: "https://github.com/quantum-one-dlt/onyx-one",
     defione:
 "https://defione.io",
-    login: "https://onyx-one-two.vercel.app/auth",
-    signup: "https://onyx-one-two.vercel.app/auth-server-action",
+    login: "https://onyx-one-git-quantumone-theblockchaincompany.vercel.app/auth",
+    signup: "https://onyx-one-git-quantumone-theblockchaincompany.vercel.app/auth-server-action",
   },
 }
