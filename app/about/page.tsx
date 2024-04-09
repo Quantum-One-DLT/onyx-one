7b5620ed00efba3d3c80c242016cd0be9847b416
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-09 17:56:21 -0400

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -59,11 +59,7 @@ export default function AboutPage() {
         </div>
         <div className="mx-auto max-w-3xl space-y-6">
           <div className="aspect-video overflow-hidden rounded-lg shadow-lg">
-<iframe className='video'
-        title='Youtube player'
-        sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
-        src={`https://youtu.be/${youtubeID}?autoplay=0`}>
-</iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/HR6a2aHhY_c?si=L2O3Cf7pQ-0HHhsP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
  </div>
 
        
