e2774df4003ae7ef50f067434887191b4617de0a
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-09 19:44:25 -0400

    Update page.tsx

--- app/defione/page.tsx
+++ app/defione/page.tsx
@@ -13,6 +13,7 @@ export default async function DeFiOnePage() {
 
         if (userSession.session) {    
   return (
+    <>
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
@@ -34,6 +35,7 @@ export default async function DeFiOnePage() {
         </Link>
       </div>
     </section>
+    </>
   )
 }
   return redirect("/auth");      
