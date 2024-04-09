fcf3369265a340a378e156553ad1aca53d7e19d7
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-09 19:38:16 -0400

    Update page.tsx

--- app/defione/page.tsx
+++ app/defione/page.tsx
@@ -6,10 +6,12 @@ import { readUserSession } from "@/utils/actions";
 import { redirect } from "next/navigation";
 import ConnectButton from "@/components/connect-button";
 
-export default async function IndexPage() {
-  const { data: userSession } = await readUserSession();
+export default async function DeFiOnePage() {
+  
 
-       
+           const { data: userSession } = await readUserSession();
+
+        if (userSession.session) {    
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
