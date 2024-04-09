79c387a7f034a47b9a2762421f4d70bedcb5f978
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-09 14:57:12 -0400

    Update promo-video.tsx

--- components/ui/promo-video.tsx
+++ components/ui/promo-video.tsx
@@ -9,7 +9,7 @@ export function PromoVideo() {
       className="overflow-hidden rounded-lg border bg-white shadow-xl"
     >
       <video autoPlay muted playsInline>
-        <source
+        <src
           ="https://youtu.be/HR6a2aHhY_c?si=D1u5cPVyi7xuDWdf"
           type="video/mp4"
         />
