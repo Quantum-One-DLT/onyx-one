ebd803d808e9ba5ee4b0baa34ec1e21512405f2e
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-10 14:06:59 -0400

    Update app

---
+++ app/signout/page.tsx
@@ -0,0 +1,11 @@
+import { logout } from "@/app/auth/actions";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import React, { useTransition } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
+export default function page() {
+
+logout();
+        
+}
