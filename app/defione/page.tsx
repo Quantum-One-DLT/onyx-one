e44403a110ca19cf12dac0ed02fe6009fa517560
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-11 00:26:01 -0400

    Update page.tsx

--- app/defione/page.tsx
+++ app/defione/page.tsx
@@ -26,22 +26,25 @@ return (
          Swap tokens and NFTs on every chain,  including BitCoin. 
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-       DeFi ONE is the first product delivery planned within our roadmap that spans 100+ years and envisages DAO driven data center ownership powering the future of public blockchains and the era of decentralized opportunity. If you are new to Quantum One DAO, connect your wallet using the button above to view your Da-Fi Token balance and verify if you were a recipient our randomized airdrop. The next 2814 new users will automatically receive an airdrop in a random amount. </p> 
+       DeFi ONE is the first product delivery planned within our roadmap that spans 100+ years and envisages DAO driven data center ownership powering the future of public blockchains and the era of decentralized opportunity. If you are new to Quantum One DAO, connect your wallet using the button above to view your Da-Fi Token balance and verify if you were a recipient of our randomized airdrop. The 1st 3k users will automatically receive an airdrop in a random amount. </p> 
 
 <p className="max-w-[700px] text-lg text-muted-foreground">In the near term, our token lock campaign milestones include: </p>
           <ol className="list-decimal list-inside space-y-2 text-lg text-muted-foreground">
 
-<li> Upgrading and changing the name of our existing DAO governance ERC20 'Da-Fi Token'</li>
+<li> Mainnet ERC20 token upgrade deployment and Zetachain deployment. Existing DAO governance 'Da-Fi Token' name and symbol will be changed for branding and marketing purposes. </li>
 
-<li> Deploying our optimized Uniswap v3 smart contracts on several chains. </li> 
+<li> Deployment of optimized Uniswap v3 smart contracts on mainnet and Zetachain. </li> 
 
-<li> Launching DeFi ONE browser and mobile DApps and mobile wallet.</li>
+<li> Live launch of DeFi ONE DApps and mobile wallet.</li>
+
+<li> Additional details will be posted here as we roll out our new website and accompany several formal AP PR's planned over the next two quarters.</li>
 </ol>
 
-<p className="max-w-[700px] text-lg text-muted-foreground">
-The total and maximum supply of the upgraded token will remain the same as Da-Fi Token at 50,000 tokens. Existing Da-Fi Token holders will automatically receive the new token that's symbol will be announced along with the planned campaign PR. The upgraded token will afford holders separate governance rights for DeFi ONE and Quantum One DA0 and the right to claim ownership of the coin associated with our planned native blockchain. The claim amount of our native coin will be directly correlated to the ratio of maximum token/native coin supply. Addditional details will be provided in our whitepaper and roadmap</p>
+<p className="max-w-[700px] text-lg text-muted-foreground"> Da-Fi Token is designated as an open blockchain network registered token and a digital consumer asset in the State of Wyoming, USA. Our SEC issued CIK number is 0001912440. The token is one of the few that is also represented off chain by legally will-able, physically printable share certificates through Carta. Wyoming law enables DAO's to operate with full, partial, or zero autonomy and, thus, great flexibility. </p>
 
-<p className="max-w-[700px] text-lg text-muted-foreground">DeFi ONE'S browser interface is live and ready to plug in our smart contract ABI's immediately after deployment. Head over to DeFi ONE, connect your wallet and view your balance. </p>
+<p className="max-w-[700px] text-lg text-muted-foreground"> The total and maximum supply of the upgraded token will remain the same at 50,000 tokens. Existing holders will automatically receive the new token that's symbol will be announced along with the planned PR campaign. </p>
+
+<p className="max-w-[700px] text-lg text-muted-foreground">DeFi ONE'S browser interface is live and ready to plug in our smart contracts to enable swaps immediately after deployment. Connect your wallet to DeFi ONE for a preview!</p>
       
          <Link
           target="_blank"
