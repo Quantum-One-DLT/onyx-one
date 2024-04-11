2501c4587407272afa264c04fdfe6c6a7b4b9359
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-10 23:12:53 -0400

    Update abi.ts

--- components/forms/abi.ts
+++ components/forms/abi.ts
@@ -6,4 +6,21 @@ export const abi = [
     inputs: [{ internalType: 'uint32', name: 'tokenId', type: 'uint32' }],
     outputs: [],
   },
+
+  {
+    type: 'function',
+    name: 'balanceOf',
+    stateMutability: 'view',
+    inputs: [{ name: 'account', type: 'address' }],
+    outputs: [{ type: 'uint256' }],
+  },
+
+  {
+    type: 'function',
+    name: 'totalSupply',
+    stateMutability: 'view',
+    inputs: [],
+    outputs: [{ name: 'supply', type: 'uint256' }],
+  },
+
 ] as const
