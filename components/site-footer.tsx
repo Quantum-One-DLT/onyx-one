af2ef85d56c7886a12499e710917f09949bfb729
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-11 23:47:45 -0400

    Update site-footer.tsx

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -2,7 +2,7 @@ import { siteConfig } from "@/config/site"
 
 export function SiteFooter() {
   return (
-    <footer className="py-4 md:px-8 md:py-0">
+    <footer className="py-1 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
           Copyright{" "}
