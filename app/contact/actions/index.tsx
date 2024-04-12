e13682f87f916c69a17a261f769355a0a25c16c3
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-12 17:28:05 -0400

    Update index.tsx

--- app/contact/actions/index.tsx
+++ app/contact/actions/index.tsx
@@ -3,3 +3,22 @@
 import { createSupbaseServerClient } from "@/utils/supaone";
 import { redirect } from "next/navigation";
 
+export async function updateInqueries(data: {
+        name: string;
+        email: string;
+        message: string;
+}) {
+        const supabase = await createSupbaseServerClient();
+
+
+const { data, error } = await supabase
+  .from('inqueries')
+  .insert([
+    { name: 'data.name', email: 'data.email', message: 'data.message'},
+  ])
+  .select()
+
+
+        const result = await supabase.updateInqueries(data);
+        return JSON.stringify(result);
+}
