654b51febed26a9e8c118d43d4f5bdb5a96b064a
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-11 22:32:14 -0400

    Update utils

---
+++ utils/send-email.ts
@@ -0,0 +1,17 @@
+import { FormData } from '@/components/forms/contact-form';
+
+export function sendEmail(data: FormData) {
+  const apiEndpoint = '/api/send';
+
+  fetch(apiEndpoint, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      alert(response.message);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
