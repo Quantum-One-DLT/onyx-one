3b0dc481d0e0057f8be64cfcb5e8f0990f1ed0da
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-12 00:21:09 -0400

    Update contact-form.tsx

--- components/forms/contact-form.tsx
+++ components/forms/contact-form.tsx
@@ -12,6 +12,7 @@ import { useForm } from 'react-hook-form'
 import { sendEmail } from '@/utils/send-email'
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { cn } from "@/lib/utils"
+import { useState } from 'react'
 
 export type FormData = {
   name: string;
@@ -22,6 +23,7 @@ export type FormData = {
 export default function ContactForm() {
 const { register, handleSubmit } = useForm<FormData>();
 
+const [isLoading, setIsLoading] = React.useState<boolean>(false)
   function onSubmit(data: FormData) {
     sendEmail(data);
   }
