bfe2c054f146b61b70707a1c4be73c93f59214c0
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-12 14:42:12 -0400

    Update supaone.tsx

--- utils/supaone.tsx
+++ utils/supaone.tsx
@@ -1,5 +1,6 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { Database } from '@/lib/supabase'
 
 export async function createSupbaseServerClientReadOnly() {
 	const cookieStore = cookies();
@@ -20,7 +21,7 @@ export async function createSupbaseServerClientReadOnly() {
 export async function createSupbaseServerClient() {
 	const cookieStore = cookies();
 
-	return createServerClient(
+	return createServerClient<Database>(
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
 		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 		{
