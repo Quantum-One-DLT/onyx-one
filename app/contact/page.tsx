eedfc401d87c4a452a1a42e9a54a9f04d6af9b15
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-13 13:56:44 -0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { cookies } from 'next/headers'
-import Contact from "@/components/forms/contact";
+import { Contact } from "@/components/forms/contact";
 import { createClient } from "@/utils/supa-server-actions";
 import { redirect } from "next/navigation";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
