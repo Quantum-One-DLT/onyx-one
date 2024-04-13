630532373e997ab695f2b42a6bb1748406fea4cd
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-12 20:31:18 -0400

    Update index.tsx

--- app/auth/actions/index.tsx
+++ app/auth/actions/index.tsx
@@ -3,6 +3,17 @@
 import { createSupbaseServerClient } from "@/utils/supaone";
 import { redirect } from "next/navigation";
 
+export async function signUpWithEmailAndPassword(data: {
+        email: string;
+        password: string;
+        confirm: string;
+}) {
+        const supabase = await createSupbaseServerClient();
+
+        const result = await supabase.auth.signUp(data);
+        return JSON.stringify(result);
+}
+
 export async function loginWithEmailAndPassword(data: {
 	email: string;
 	password: string;
