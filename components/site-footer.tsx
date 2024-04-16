85fc03075de5c47d73c13f66c5417383feea9152
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-16 17:06:46 -0400

    Update site-footer.tsx

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -3,14 +3,14 @@ import { siteConfig } from "@/config/site"
 export function SiteFooter() {
   return (
     <footer className="py-1 md:px-8 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+      <div className="container flex flex-col items-center text-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-xs font-medium leading-loose text-muted-foreground">
-          Copyright{" "}
+          ©{" "}
           <a
             href={siteConfig.links.github}
             target="_blank"
             rel="noreferrer"
-            className="text-xs font-medium"
+            className="text-xs text-center font-medium"
           >
             Quantum One DAO LLC 2024
           </a>
