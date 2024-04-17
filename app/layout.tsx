a846cd47debb3638763a852568c96c7e356a5c22
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-16 23:44:36 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,6 @@ export const metadata: Metadata = {
   referrer: 'origin-when-cross-origin',
   keywords: ['DAO', 'Quantum One DAO', 'Quantum One', 'DEX', 'BitCoin', 'DeFi ONE', 'DAO owned data centers', 'quantum blockchain', 'Ethereum', 'DeFi', 'omni-chain defi','BitCoin smart contracts', 'ERC20', 'governance token', 'DEX governance token', 'open blockchain network', 'digital consumer assets', 'Wyoming DAO', 'Ethereum', 'DeFi Llama', 'swap token and nfts', 'swap on any chain', 'BitCoin DeFi', 'engineering opportunity', 'token lock campaign', 'Zetachain', 'SAFE', 'quantum based blockchain networks', 'decentralized opportunity', 'Da-Fi Token', 'wallet connect', 'github', 'Uniswap v3 smart contracts', 'TVL', 'post quantum cryptography', 'post quantum cryptocurrency', 'decentralized ai', 'blockchain ai', 'ai', 'decentralized compliance protocols'],
   authors: [{ name: 'Robert Mourey Jr' },
-  applicationName: 'Quantum One', 
   creator: 'Robert Mourey Jr',
   publisher: 'Quantum One DAO', 
   formatDetection: {
