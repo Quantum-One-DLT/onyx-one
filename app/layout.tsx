12150f6e6a62cd2d835aa2aa70b23e835039ffbf
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-17 00:45:30 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import './globals.css'
-import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import type { Metadata,Viewport } from 'next'
 import { headers } from 'next/headers'
