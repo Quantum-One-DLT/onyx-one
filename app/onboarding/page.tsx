e98fcfc5d90efbeaa140494419f9248e663c89df
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-17 16:28:04 -0400

    Update page.tsx

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -9,21 +9,7 @@ import { buttonVariants } from "@/components/ui/button"
 import AuthForm from "@/app/auth/components/AuthForm"
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-
-  openGraph: {
-    images: 'https://quantumone.b-cdn.net/onyx/og-image.jpg',
-  },
+  title: 'Onboarding', 
 }
 
 export default function OnboardingPage() {
