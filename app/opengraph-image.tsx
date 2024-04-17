90dac2040fa0f29dd456c73b2ae511f3f23ff96f
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-17 00:50:35 -0400

    Update opengraph-image.tsx

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -33,14 +33,6 @@ export default async function Image() {
       // For convenience, we can re-use the exported opengraph-image
       // size config to also set the ImageResponse's width and height.
       ...size,
-      fonts: [
-        {
-          name: 'Inter',
-          data: await interSemiBold,
-          style: 'normal',
-          weight: 400,
-        },
-      ],
     }
   )
 }
