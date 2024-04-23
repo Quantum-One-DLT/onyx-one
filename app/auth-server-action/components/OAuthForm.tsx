f5b06b7e6f89461ed7582f494042cc0958b9fd8a
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-22 21:44:41 -0400

    Update OAuthForm.tsx

--- app/auth-server-action/components/OAuthForm.tsx
+++ app/auth-server-action/components/OAuthForm.tsx
@@ -2,5 +2,8 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function OAuthForm() {
-	return <Button className="w-full">Login With Github</Button>;
+	return 
+      <div>
+        <form action="/auth/oauth/google" method="post">
+<Button variant="outline">Login With Google</Button>;
 }
