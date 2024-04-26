0282f127a658779eb401c2b7121ef72635efb9ae
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-26 13:52:43 -0400

    Update page.tsx

--- app/auth-server-action/page.tsx
+++ app/auth-server-action/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { AuthFormLegacy } from '@/app/auth-server-action/components/AuthFormLegacy'
+import { readUserSession } from "@/utils/actions";
 
 export default function page() {
+        const { data: userSession } = await readUserSession();
+
+        if (userSession.session) {
+                return redirect("/account");
+        }
 	return (
 		<div className="flex justify-center items-center h-screen">
 			<div className="w-96">
