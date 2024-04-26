75083ed257dc132b0fa706413bdb1ded1c298196
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-26 13:31:14 -0400

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -15,7 +15,7 @@ data: { user },} = await supabase.auth.getUser()
   return (
     <div className="mt-4 px-4 py-8 space-y-8">
     <div className="mx-auto px-2 flex w-full flex-col justify-center space-y-2">
-        <h1 className="text-lg font-medium text-center">Account Details</h1>
+        <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Account Profile</h1>
         <p className="text-sm text-justified-center text-muted-foreground">
           AirDrops are sent to your Airdrop Wallet Address. To add or update this address, connect your wallet, copy and paste your Connected Wallet Address, and use the update button to save your changes. 
         </p>
