36900d49c3655240b5fa9cca10937e670fd1d366
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-26 16:00:44 -0400

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -17,8 +17,17 @@ data: { user },} = await supabase.auth.getUser()
     <div className="mx-auto px-2 flex w-full flex-col justify-center space-y-2">
         <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Account Profile</h1>
         <p className="text-sm text-justified-center text-muted-foreground">
-          AirDrops are sent to your Airdrop Wallet Address. To add or update this address, connect your wallet, copy and paste your Connected Wallet Address, and use the update button to save your changes. 
-        </p>
+          AirDrops are sent to your Airdrop Wallet Address. To add or update this address, connect your wallet, copy and paste your Connected Wallet Address, and use the update button to save your changes.</p>
+
+        <Link
+          href="/defione"
+          target="_blank"
+          rel="noreferrer"
+          className="text-1xl font-bold tracking-tighter sm:text-3xl"
+        >
+         Learn about DeFi ONE 
+        </Link>
+     
       </div>
       <Separator />
       <AccountForm user={user}/>
