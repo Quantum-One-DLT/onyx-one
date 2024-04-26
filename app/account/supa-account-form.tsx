e5a229893d15406c5c3a6aac95599e70ed259747
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-26 10:15:30 -0400

    Update supa-account-form.tsx

--- app/account/supa-account-form.tsx
+++ app/account/supa-account-form.tsx
@@ -152,7 +152,7 @@ const { address } = useAccount()
 <input 
 className={cn("flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50")}
 id="waddress" type="text" value={waddress || ''} onChange={(e) => setWaddress(e.target.value)}/>
-<label htmlFor="connectedAddress">Connected Wallet Address</label>
+<label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" htmlFor="connectedAddress">Connected Wallet Address</label>
       <div className="flex flex-col">{ensAvatar && <img alt="ENS Avatar" src={ensAvatar} />}
       {address && <div className="flex flex-col">{ensName ? `${ensName} (${address})` : address}</div>}
       <button           className={buttonVariants({ variant: "outline" })} onClick={() => disconnect()}>Disconnect Wallet</button>
