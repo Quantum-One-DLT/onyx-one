fff3cd93c8740fd925c735d5d774a1552bee523c
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-27 22:22:54 -0400

    Update page.tsx

--- app/daccount/page.tsx
+++ app/daccount/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi'
 import { buttonVariants } from "@/components/ui/button"
+import { Avatar } from '@/components/ui/avatar'
 
 export default function Daccount () {
   const { address } = useAccount()
@@ -11,6 +12,7 @@ export default function Daccount () {
   return (
     <div className="container items-centered">
 <div className="flex flex-col items-centered">
+<Avatar />
       <div>{ensAvatar && <img alt="ENS Avatar" src={ensAvatar} />}
       {address && <div>{ensName ? `${ensName} (${address})` : address}</div>}
       <button           className={buttonVariants({ variant: "outline" })} onClick={() => disconnect()}>Disconnect</button>
