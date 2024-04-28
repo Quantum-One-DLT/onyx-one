d5779d482dc85ca6a0fd5424533aec29438d9c96
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-27 22:05:49 -0400

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,6 +2,7 @@ import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 import { ProfileForm } from '@/components/forms/profile-form'
 import { createClient } from '@/utils/supa-server-actions'
+import { Avatar } from '@/components/ui/avatar'
 
 export default async function ProfilePage() {
   const cookieStore = cookies()
@@ -13,4 +14,5 @@ export default async function ProfilePage() {
   }
 
   return <p className="mt-10 px-4 py-8 text-center font-bold">Hello {data.user.email}! Your complete profile is coming soon!</p>
+<Avatar/>
 }
