6257d5422c831512a6364c14d79f1d60124d4dbf
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-27 22:02:48 -0400

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -149,7 +149,7 @@ defione: (props: LucideProps) => (
   ),
 avatardefault: (props: LucideProps) => (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-        class="h-20 w-20 p-5 text-white bg-gray-500 stroke-currentColor">
+        class="h-20 w-20 p-5 text-white bg-gray-500 stroke-currentColor" {...props}>
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
             d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
     </svg>
