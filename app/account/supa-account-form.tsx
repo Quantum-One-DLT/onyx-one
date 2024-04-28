ab42cfb0df7547eb004b21a769365edcf4b1f96a
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-27 21:52:51 -0400

    Update supa-account-form.tsx

--- app/account/supa-account-form.tsx
+++ app/account/supa-account-form.tsx
@@ -163,7 +163,7 @@ id="waddress" type="text" value={waddress || ''} onChange={(e) => setWaddress(e.
       {address && <div className={cn("flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 overflow-x-auto hover:overflow-x-scroll")}>{ensName ? `${ensName} (${address})` : address}</div>}
  </div>
 </div>
-<div className="flex gap-4">    
+<div className="flex w-full gap-4">    
 <button           className={buttonVariants({ variant: "outline" })} onClick={() => disconnect()}>Disconnect Wallet</button>
    
     
