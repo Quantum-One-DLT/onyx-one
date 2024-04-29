35db84395fb97b56fb1f580ee3b87025c580f20c
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-29 15:08:13 -0400

    Update components

---
+++ components/cookie-button.tsx
@@ -0,0 +1,24 @@
+ 
+
+
+
+
+<Link
+              href="#'
+              target="_blank"
+              rel="noreferrer"
+              className="absolute bottom-0 right-0"
+            >
+              <div
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "ghost",
+                })}
+              >
+                
+       <a href="#" className={cn("termly-display-preferences")}></a>
+     
+                <Icons.cookie className="h-5 w-5" />
+                <span className="sr-only"></span>
+              </div>
+            </Link>
