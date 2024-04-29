660faf3e97fb99720af3867c310a4779a4398f9e
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-29 16:25:04 -0400

    Update cookie-button.tsx

--- components/cookie-button.tsx
+++ components/cookie-button.tsx
@@ -8,7 +8,7 @@ import { cn } from '@/lib/utils'
 export function CookieButton() {
 
 return (
-<div className="sticky absolute bottom-0 right-0 z-50">
+<div className="fixed bottom-0 right-0 z-50">
 
               <div
                 className={buttonVariants({
