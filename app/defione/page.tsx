050bc932433982f82268dafeee116fb3608b2667
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-29 17:01:27 -0400

    Update page.tsx

--- app/defione/page.tsx
+++ app/defione/page.tsx
@@ -19,7 +19,7 @@ const { data: userSession } = await readUserSession();
  }   
 
 return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="container mx-auto grid justify-items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-1">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           DeFi ONE <br className=" xs:inline" />
