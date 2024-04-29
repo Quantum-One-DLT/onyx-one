49ae1697b96469a0f1fa50099c5bb1627c41b608
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-04-29 15:06:42 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -146,25 +146,7 @@ export default function RootLayout({
 <Script
   type="text/javascript"
   src="https://app.termly.io/resource-blocker/a49f36df-8d64-46a3-9ef6-253dcebfaaf0?autoBlock=off"/>
-        <Link
-              href="#'
-              target="_blank"
-              rel="noreferrer"
-              className="absolute bottom-0 right-0"
-            >
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
-              >
-                
-       <a href="#" className={cn("termly-display-preferences")}></a>
-     
-                <Icons.cookie className="h-5 w-5" />
-                <span className="sr-only"></span>
-              </div>
-            </Link>
+       
 </ThemeProvider>
 
       </body>
