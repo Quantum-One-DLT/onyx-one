1904ea54fbfe358cb9b45027ccc2ae408c89d2f5
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-05-08 15:37:51 -0400

    Update page.tsx

--- app/defione/page.tsx
+++ app/defione/page.tsx
@@ -21,7 +21,7 @@ const { data: userSession } = await readUserSession();
 
 return (
     <section className="container mx-auto grid justify-evenly justify-items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col justify-evenly justify-items-center gap-1">
+      <div className="flex max-w-[980px] flex-col justify-evenly justify-items-center gap-1 space-y-8">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           DeFi ONE <br className=" xs:inline" />
          Swap tokens and NFTs on every chain,  including BitCoin. 
