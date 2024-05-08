cae44b23055b217ad46e1a014ea8fba0cfee5d74
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-05-08 14:07:45 -0400

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -29,7 +29,7 @@ export default function AboutPage() {
         <div className="flex flex-col items-center justify-center space-y-6 text-center">
           <div className="space-y-3">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Our Mission</h1>
-            <p className="max-w-[800px] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-justify"> Our overarching vision views the rapidly approaching quantum and photonic computing era as an era of greater decentralized opportunity, a new paradigm. We seek to unite the blockchain community to reclaim its independence from cloud service providers by leveraging its $2 trillion plus USD market capitalization to build pre and post quantum capable, eco-friendly, 'DAO owned' data centers and the cornerstone of future public blockchain technology. Simultaneously, we are committed to advancing global economic prosperity by promoting financial inclusion, security, autonomy, and asset ownership. 
+            <p className="max-w-[980px] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-justify"> Our overarching vision views the rapidly approaching quantum and photonic computing era as an era of greater decentralized opportunity, a new paradigm. We seek to unite the blockchain community to reclaim its independence from cloud service providers by leveraging its $2 trillion plus USD market capitalization to build pre and post quantum capable, eco-friendly, 'DAO owned' data centers and the cornerstone of future public blockchain technology. Simultaneously, we are committed to advancing global economic prosperity by promoting financial inclusion, security, autonomy, and asset ownership. 
             </p>
             <Separator/>
           </div>
@@ -40,7 +40,7 @@ export default function AboutPage() {
             <h1 className="text-3xl font-bold tracking-tighter text-center sm:text-5xl">Our Values</h1>
 </div>
 </div>
-        <div className="max-w-[800px] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-justify mx-auto space-y-8">
+        <div className="max-w-[980px] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-justify mx-auto space-y-8">
           <ol className="list-decimal list-inside space-y-3">
             
              <li>Decentralization: Embrace the decentralized nature of decision-making, allowing all stakeholders to have a voice in governance processes.</li>
@@ -67,10 +67,10 @@ export default function AboutPage() {
       
      
           <div className="space-y-8">
-          <h5 className="max-w-[800px] place-self-center text-center text-3xl font-bold tracking-tighter sm:text-5xl"> Why are Gemini and ChatGPT Bullish on DAO Owned Data Centers?</h5>
+          <h5 className="max-w-[980px] place-self-center text-center text-3xl font-bold tracking-tighter sm:text-5xl"> Why are Gemini and ChatGPT Bullish on DAO Owned Data Centers?</h5>
 
          
-           <div className="place-self-center max-w-[800px] aspect-video overflow-hidden rounded-lg shadow-lg ">
+           <div className="place-self-center max-w-[980px] aspect-video overflow-hidden rounded-lg shadow-lg ">
 <iframe width="100%" height="100%" src="https://www.youtube.com/embed/HR6a2aHhY_c?si=L2O3Cf7pQ-0HHhsP" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
  
           </div>
