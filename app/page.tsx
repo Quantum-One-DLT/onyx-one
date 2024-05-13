ad6eec45bf6c203ff8c6babbbc4969e0f52bf8e3
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-05-13 10:02:13 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default async function IndexPage() {
 		return redirect("/account");
 	}
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="container mx-auto grid items-center gap-6 pb-8 pt-6 px-4 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Opportunity is<br className=" xs:inline" />
