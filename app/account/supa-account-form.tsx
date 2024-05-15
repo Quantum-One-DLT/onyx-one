511c9ced01d5cfc6aac0da98018207cd4fb4c555
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-05-15 14:43:21 -0400

    Update supa-account-form.tsx

--- app/account/supa-account-form.tsx
+++ app/account/supa-account-form.tsx
@@ -144,7 +144,7 @@ const { address } = useAccount()
         />
       </div>
       <div className="flex flex-col">
-        <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" htmlFor="username">Twitter | X Username</label>
+        <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" htmlFor="xhandle">X Username</label>
         <input className={cn("flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50")}
           id="xhandle"
           type="text"
