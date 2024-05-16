269378b78b543f3e9206270da6c956117588c94b
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-05-16 15:06:05 -0400

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -14,7 +14,7 @@ import ConnectButton from "./connect-button"
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between xs:space-x-0">
         <MainNav items={siteConfig.mainNav} />
     <MobileNav />    
 <div className="flex flex-1 items-center justify-end space-x-4">
