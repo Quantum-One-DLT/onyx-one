ede9dbf202734ff633c35f8a30fde4a1de7324ae
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-05-15 22:44:16 -0400

    Update supa-account-form.tsx

--- app/account/supa-account-form.tsx
+++ app/account/supa-account-form.tsx
@@ -8,6 +8,7 @@ import { type User } from '@supabase/supabase-js'
 import Avatar from './avatar'
 import { Input } from '@/components/ui/input'
 import { Checkbox } from '@/components/ui/checkbox'
+import { Label } from '@/components/ui/label'
 
 export default function AccountForm({ user }: { user: User | null }) {
   const supabase = useSupabaseBrowser()
