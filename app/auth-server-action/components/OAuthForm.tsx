f4722af8f0df13d5abd92a876570c601dff87056
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-05-21 19:47:44 +0000

    .

--- app/auth-server-action/components/OAuthForm.tsx
+++ app/auth-server-action/components/OAuthForm.tsx
@@ -2,11 +2,11 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function OAuthForm() {
-	return 
+	return (
       <div>
         <form action="/auth/oauth/google" method="post">
 <Button className="w-full items-centered gap-2" variant="outline">Login With Google</Button>
 </form>
 </div>
-;
+  );
 }
