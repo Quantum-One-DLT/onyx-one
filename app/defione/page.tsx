8de740a41744fd4bb3cfffa453b12e4b883cf74e
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-05-22 18:31:38 -0400

    Update page.tsx

--- app/defione/page.tsx
+++ app/defione/page.tsx
@@ -29,16 +29,16 @@ return (
         <p className="max-w-[980px] text-lg text-justified">
        DeFi ONE (DFI1) is the first product delivery planned within our roadmap that spans 100+ years and envisages DAO driven data center ownership powering the future of public blockchains and the era of decentralized opportunity. If you are new to Quantum One DAO, connect your wallet using the button above to view your Da-Fi Token balance and verify if you were a recipient of our randomized airdrop. The 1st 3K Quantum One users will automatically receive an airdrop of our governance token in a random amount. </p> 
 
-<p className="max-w-[980px] text-lg text-justify">
+<p className="max-w-[980px] text-lg">
 DFI1 is designed to utilize the interoperability and abstractability of ZetaChain and augmented with an AI layer compromised of programmable, cross-chain messaging capable autonomous agents to enable token, NFT, and native-to-native coin swaps on every chain, including BitCoin. After launch, we will implement bug bounty programs designed to motivate talented individuals and teams to find security vulnerabilities and ideate new algorithms and protocols within classic, quantum and photonic computing scopes. We've been publicly warning the blockchain community about quantum hacks and compliance concerns for nearly 2.5 years. Considering Ripple just concluded the same about quantum hacks in May 2024, Ethereum founder Vitalik Buterin warned of a possible quantum era related hard fork in October 2023, and companies such as IonQ claim quantum advantage is imminent, we're aiming to shift the entire industry into a proactive and innovative classic, quantum, photonic, compliant mindset. </p>
 
-<p className="max-w-[980px] text-lg text-justify">In the near term, we are finalizing plans for a token sale campaign to help fund the launch of DFI1. The final details will be formally announced via press release in the next few weeks. DFI1's user interface is live and ready for developers to swap in our smart contract addresses and live API keys upon deployment. Currently, we anticipate including the following goals within the formal campaign announcement: 
+<p className="max-w-[980px] text-lg">In the near term, we are finalizing plans for a token sale campaign to help fund the launch of DFI1. The final details will be formally announced via press release in the next few weeks. DFI1's user interface is live and ready for developers to swap in our smart contract addresses and live API keys upon deployment. Currently, we anticipate including the following goals within the formal campaign announcement: 
 </p>
       <ol className="list-decimal list-inside space-y-2 text-lg text-justify">
 
 <li> Upgrade smart contracts for our existing ERC20 DAO governance token symbol DA-FI and deployment of upgraded ERC20 token on ZetaChain. </li>
 
-<li> Deployment of optimized Uniswap v3 smart contracts on mainnet and Zetachain. </li> 
+<li> Deployment of optimized Uniswap v3 smart contracts on Zetachain. </li> 
 
 <li> Live launch of DeFi ONE PWA and mobile DApps.</li>
 
@@ -53,7 +53,7 @@ DFI1 is designed to utilize the interoperability and abstractability of ZetaChai
 </ol>
 
 <Separator/>
-<p className="max-w-[980px] text-lg text-justify"> Access the DFI1 demo using the button below! Please note that demos are not considered fully tested, functional applications. 
+<p className="max-w-[980px] text-lg"> Access the DFI1 demo using the button below! Please note that demos are not considered fully tested, functional applications. 
 </p>
       
          <Link
