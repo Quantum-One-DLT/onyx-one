8312384d3a44f78d8ee8291adbfd99e101c576e1
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-15 12:19:58 -0400

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -21,7 +21,7 @@ export function SiteHeader() {
     
 <MobileNav/>
 
-<div className="flex flex-1 items-center justify-between space-x-2 xs:items-end md:justify-end"> 
+<div className="flex md:flex-1 items-center justify-between space-x-2 xs:items-end> 
 <nav className="flex items-center">
            <ConnectButton/>
         <Link
