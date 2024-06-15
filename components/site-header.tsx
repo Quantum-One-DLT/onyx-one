aeae90f2b21394031b61f63f29bd45f7e7faefbb
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-15 10:19:51 -0400

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -13,12 +13,13 @@ import ConnectButton from "./connect-button"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 ">
-      <div className="container flex h-14 max-w-screen-2xl items-center sm:justify-between xs:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
-    <MobileNav />
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+<div className="container flex h-14 max-w-screen-2xl items-center sm:justify-between xs:space-x-0">
+ <MainNav items={siteConfig.mainNav} />
+    <MobileNav/>
+
 <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end"> 
-          <nav className="flex items-center space-x-1">
+<nav className="flex items-center space-x-1">
            <ConnectButton/>
         <Link
               href={siteConfig.links.github}
