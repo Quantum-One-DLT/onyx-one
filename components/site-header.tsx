158e57c2a95bf985493234ed312d228068c83299
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-15 11:41:16 -0400

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -15,14 +15,14 @@ export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
 
-<div className="container flex h-14 max-w-screen-2xl items-center sm:justify-between xs:space-x-0">
+<div className="container flex h-14 max-w-screen-2xl items-center">
  
 <MainNav items={siteConfig.mainNav} />
     
 <MobileNav/>
 
 <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end"> 
-<nav className="flex items-center space-x-1">
+<nav className="flex items-center">
            <ConnectButton/>
         <Link
               href={siteConfig.links.github}
