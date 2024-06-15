73854206fcc698e10e87983b168fba41e86cbb74
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-15 13:41:07 -0400

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { cn } from "@/lib/utils"
+
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
@@ -13,16 +13,12 @@ import ConnectButton from "./connect-button"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-
-<div className="container flex h-14 max-w-screen-2xl items-center">
- 
-<MainNav items={siteConfig.mainNav} />
-    
-<MobileNav/>
-
-<div className="flex flex-1 items-center justify-between space-x-2 md:justify-end"> 
-<nav className="flex items-center">
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between xs:space-x-0">
+        <MainNav items={siteConfig.mainNav} />
+    <MobileNav />    
+<div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-1">
            <ConnectButton/>
         <Link
               href={siteConfig.links.github}
@@ -30,14 +26,12 @@ export function SiteHeader() {
               rel="noreferrer"
             >
               <div
-                className={cn(buttonVariants({
+                className={buttonVariants({
                   size: "icon",
                   variant: "ghost",
-                }),
-               "w-9 px-0"
-                )}
+                })}
               >
-                <Icons.gitHub className="h-4 w-4" />
+                <Icons.gitHub className="h-5 w-5" />
                 <span className="sr-only">GitHub</span>
               </div>
             </Link>
@@ -47,14 +41,12 @@ export function SiteHeader() {
               rel="noreferrer"
             >
               <div
-                className={cn(buttonVariants({
+                className={buttonVariants({
                   size: "icon",
                   variant: "ghost",
-                }),
-                "w-9 px-0"
-               )}
+                })}
               >
-                <Icons.discord className="h-4 w-4" />
+                <Icons.discord className="h-5 w-5" />
                 <span className="sr-only">Discord</span>
               </div>
             </Link>
