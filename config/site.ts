06497e50adc2cd2ad9bd37cfb0b9f8b711f680b7
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-06-18 15:26:45 -0400

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -40,6 +40,8 @@ export const siteConfig = {
     dafietherscan:"https://etherscan.io/token/0xeaad65885fea47a3b1258935f4ce83aab06fdd3a",
     qonepaper: "https://www.notion.so/Quantum-One-Research-and-Pitch-Paper-9c2497e7f4534ee08eb2f25d131d7ed5",
     discord: "https://discord.com/invite/AJw2bDsCNh",
+    v1factory: 
+"https://sepolia.etherscan.io/address/0xea15642a49bfcde8a8dbf97ee6fd882d0a9e9017#code"
 
   },
 
