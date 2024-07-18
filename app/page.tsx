82bc91192f242e0347c759a8b3d133a7bee6bc3b
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-07-18 18:35:56 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default async function IndexPage() {
 		return redirect("/account");
 	}
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 px-4 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
+    <section className="container mx-auto flex flex-1 flex-col items-center justify-center gap-6 pb-8 pt-6 px-4 py-12 md:px-6 md:py-10 lg:py-24">
+      <div className="flex max-w-[980px] flex-col text-center gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           The chain-agnostic DAO for a post quantum world. 
 	</h1>
