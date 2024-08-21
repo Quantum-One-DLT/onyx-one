11ed542c5756895f5f508928e21c9a4ab752cd8c
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-08-20 22:55:19 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,54 @@ const withMDX = require('@next/mdx')()
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
 });
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.supabase.co googleapis.com *.walletconnect.com;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src *.supabase.co quantumone.b-cdn.net *.unsplash.com youtube.com;
+  connect-src *;
+  font-src 'self' googleapis.com;
+  frame-src *.supabase.co youtube.com
+`
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+]
 
 const nextConfig = {
   webpack: config => {
@@ -40,16 +88,16 @@ const nextConfig = {
         port: '',
         pathname: '/embed/HR6a2aHhY_c?si=L2O3Cf7pQ-0HHhsP',
       },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
 
       {
         protocol: 'https',
         hostname: 'quantumone.b-cdn.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
       },
@@ -59,14 +107,21 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cloud.walletconnect.com',
         port: '',
-      
+
       },
     ],
   },
+   async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: securityHeaders,
+        },
+      ]
+    },
+
 
    pageExtensions: ['ts', 'tsx', 'mdx', 'js', 'jsx'],
 }
 
 module.exports = withMDX(nextConfig)
-
-
