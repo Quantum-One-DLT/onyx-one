adf3334ab4d7fd459368a0c36d18beffb8746c70
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-04-20 14:06:39 -0400

    Update page.tsx

--- app/ssrcountries/[id]/page.tsx
+++ app/ssrcountries/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
 import { prefetchQuery } from '@supabase-cache-helpers/postgrest-react-query'
-import useSupabaseServer from '@/utils/supabase-server'
+import { SupabaseServerAsync } from '@/utils/supabase-server'
 import { cookies } from 'next/headers'
 import Country from './country'
 import { getCountryById } from '@/queries/country-by-id'
@@ -8,7 +8,7 @@ import { getCountryById } from '@/queries/country-by-id'
 export default async function CountryPage({ params }: { params: { id: number } }) {
   const queryClient = new QueryClient()
   const cookieStore = cookies()
-  const supabase = await useSupabaseServer(cookieStore)
+  const supabase = await SupabaseServerAsync(cookieStore)
 
   await prefetchQuery(queryClient, getCountryById(supabase, params.id))
 
