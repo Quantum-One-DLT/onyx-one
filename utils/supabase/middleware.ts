28bb8c65c1dbb2b38a2dbe3e518ca1d1e949b572
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-04-25 15:07:31 -0400

    Update middleware.ts

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -4,7 +4,7 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({
+  let response = NextResponse.next({
     request: {
       headers: request.headers,
     },
@@ -58,6 +58,5 @@ export async function updateSession(request: NextRequest) {
 
   await supabase.auth.getUser()
 
-  return supabaseResponse
+  return response
 }
-
